1d4b7859148bec81baf08b1f5c5ec185888dc6b1
huskyhehe <97263406+huskyhehe@users.noreply.github.com>
2023-10-06 19:31:21 -0400

    Create Functions.cpp

---
+++ intro/Functions.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cin >> a >> b >> c >> d;
+    cout << max({a, b, c, d});
+    return 0;
+}
